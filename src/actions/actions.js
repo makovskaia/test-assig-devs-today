90dd36ebd3d884541c7102eb212b6c591bb4ee76
mawa <makovskayamd@gmail.com>
2019-09-12 10:27:43 +0300

    more formatting

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,39 +1,39 @@
 export const addPosts = posts => {
-  return ({
-    type: 'ADD_POSTS',
+  return {
+    type: "ADD_POSTS",
     posts
-  })
-}
+  };
+};
 
 export const toggleIsFetching = bool => {
-  return ({
-    type: 'TOGGLE_ISFETCHING',
-    isFetching: bool,
-  })
-}
+  return {
+    type: "TOGGLE_ISFETCHING",
+    isFetching: bool
+  };
+};
 
 export const fetchPostsWin = response => dispatch => {
-  return dispatch(addPosts(response))
-}
+  return dispatch(addPosts(response));
+};
 
 export const fetchPostsFail = error => {
-  return ({
-    type: 'FETCH_POSTS_FAIL',
-    error,
-  })
-}
+  return {
+    type: "FETCH_POSTS_FAIL",
+    error
+  };
+};
 
 export function fetchPosts() {
   return async dispatch => {
     try {
       let a = dispatch(toggleIsFetching(true));
-      let response = await fetch('https://simple-blog-api.crew.red/posts')
-      let res2 = await response.json()
-      let res3 = await dispatch(fetchPostsWin(res2))
-      let res4 = await dispatch(toggleIsFetching(false))
-    } catch(error) {
-      dispatch(fetchPostsFail(error))
+      let response = await fetch("https://simple-blog-api.crew.red/posts");
+      let res2 = await response.json();
+      let res3 = await dispatch(fetchPostsWin(res2));
+      let res4 = await dispatch(toggleIsFetching(false));
+    } catch (error) {
+      dispatch(fetchPostsFail(error));
     }
-    return 'ok'
-  }
+    return "ok";
+  };
 }
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,20 +1,20 @@
 const initState = {
   posts: [],
-  isFetching: false,
-}
+  isFetching: false
+};
 const posts = (state = initState, action) => {
   switch (action.type) {
-    case 'TOGGLE_ISFETCHING':
-      return { ...state, isFetching: action.isFetching }
-    case 'ADD_POSTS':
-      return { ...state, posts: action.posts }
-    case 'FETCH_POSTS_FAIL':
-      return { ...state, isFetching: false }
-    case 'FETCH_POSTS_WIN':
-      return { ...state, isFetching: false, posts: action.response }
+    case "TOGGLE_ISFETCHING":
+      return { ...state, isFetching: action.isFetching };
+    case "ADD_POSTS":
+      return { ...state, posts: action.posts };
+    case "FETCH_POSTS_FAIL":
+      return { ...state, isFetching: false };
+    case "FETCH_POSTS_WIN":
+      return { ...state, isFetching: false, posts: action.response };
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default posts
+export default posts;
